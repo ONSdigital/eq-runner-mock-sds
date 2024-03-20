d3301402a632021f6a09e6538ce137b4068619ff
Liam Toozer <liam.toozer@gmail.com>
2024-03-20 13:14:39 +0000

    Update end points. Use inner functions for path traversal for JSON files. Handle unit data

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+from copy import deepcopy
 from datetime import datetime, timezone, timedelta
 from functools import lru_cache
 from pathlib import Path
@@ -13,24 +14,21 @@ from pydantic import BaseModel
 from sdc.crypto.jwe_helper import JWEHelper
 from sdc.crypto.key_store import KeyStore
 
-KEY_PURPOSE_SDS = "supplementary_data"
-MOCK_DATA_PATH = Path(__file__).parent.parent / "mock_data"
 
 app = FastAPI()
 
 with open("dev-keys.yml", encoding="UTF-8") as keys_file:
     keys = KeyStore(yaml.safe_load(keys_file))
 
+KEY_PURPOSE_SDS = "supplementary_data"
+MOCK_DATA_ROOT_PATH = Path(__file__).parent.parent / "mock_data"
+PERIOD_ID = "201605"  # matches Launcher period_id
 
-SURVEY_ID_TO_MOCK_DATA_FOLDER_MAPPING = {
-    "221": "bres_and_brs",
-    "241": "bres_and_brs",
-    "014": "prodcom",
-    "068": "roofing_tiles_slate_sand_and_gravel",
-    "071": "roofing_tiles_slate_sand_and_gravel",
-    "066": "roofing_tiles_slate_sand_and_gravel",
-    "076": "roofing_tiles_slate_sand_and_gravel",
-    "123": "test",
+MOCK_DATA_PATHS_BY_SURVEY_ID = {
+    "bres_and_brs": ["221", "241"],
+    "prodcom": ["014"],
+    "roofing_tiles_slate_sand": ["068", "076", "077", "076"],
+    "test": "123",
 }
 
 
@@ -39,6 +37,7 @@ class Metadata(BaseModel):
     Model for SDS Metadata Response
     copied from https://github.com/ONSdigital/sds/blob/main/src/app/models/dataset_models.py#L19
     """
+
     survey_id: str
     period_id: str
     form_types: list[str]
@@ -49,45 +48,45 @@ class Metadata(BaseModel):
     filename: str
     dataset_id: UUID
     title: str | None = None
-    data: list[dict]    # additional eQ-only field for mapping the mocked unit data
+    data: dict  # additional eQ-only field for mapping the mocked unit data
 
 
 @app.get("/v1/unit_data")
-def get_sds_data(
-    dataset_id: UUID, identifier: str = Query(min_length=1)
-) -> MutableMapping:
+def get_sds_data(dataset_id: UUID, identifier: str = Query(min_length=1)) -> dict:
     # The mock current does not make use of identifier
+    metadata_collection = load_mock_data()
 
-    # TODO: This map is now created earlier on
-    # guid_filename_map = {
-    #     "c067f6de-6d64-42b1-8b02-431a3486c178": "supplementary_data",
-    #     "693dc252-2e90-4412-bd9c-c4d953e36fcd": "supplementary_data_v2",
-    #     "9b418603-ba90-4c93-851a-f9cecfbda06f": "supplementary_data_v3",
-    # }
-
-
-    # have a dict that holds identifier and datasset_id for lookups - dynamically (either a tuple or a dict)
-    # ensure that the identifier matches between launcher, runner
-
-    if filename := guid_filename_map.get(str(dataset_id)):
-        return encrypt_mock_data(load_mock_data(f"mock_data/{filename}.json"))
-
+    for metadata in metadata_collection:
+        if metadata.dataset_id == dataset_id:
+            if unit_data := metadata.data.get(identifier):
+                return unit_data
     raise HTTPException(status_code=404)
 
 
 @app.get("/v1/dataset_metadata")
 def get_sds_dataset_ids(
     survey_id: str = Query(min_length=1), period_id: str = Query(min_length=1)
-) -> list[dict]:
+) -> list[Metadata]:
     # The mock currently does not make use of period_id
-    return load_mock_sds_dataset_metadata(survey_id)
+    metadata_collection = (
+        load_mock_data()
+    )  # TODO: As we're modifying the object, make a deepcopy first??
+
+    if filtered_metadata_by_survey_id := [
+        metadata
+        for metadata in metadata_collection
+        if metadata.survey_id == survey_id and not delattr(metadata, "data")
+    ]:
+        return filtered_metadata_by_survey_id
+    raise HTTPException(status_code=404)
 
 
 def get_mocked_chronological_date(schema_version: str) -> str:
     """Using the schema version, we can ensure mocked dates appear 'chronological'"""
     base_date = "2021-01-01T00:00:00Z"
-
-    chronological_date = datetime.fromisoformat(base_date) + timedelta(weeks=get_version_number(schema_version))
+    chronological_date = datetime.fromisoformat(base_date) + timedelta(
+        weeks=get_version_number(schema_version)
+    )
     return chronological_date.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
@@ -99,7 +98,9 @@ def get_json_file_count_from_path(path: Path) -> int:
     return sum(1 for _ in path.glob("*.json"))
 
 
-def build_sds_data(*, survey_id: str, period_id: str, dataset_id: UUID, path: Path) -> Metadata:
+def build_sds_data(
+    *, survey_id: str, period_id: str, dataset_id: UUID, path: Path
+) -> Metadata:
     """
     Add docstring here
     """
@@ -107,7 +108,7 @@ def build_sds_data(*, survey_id: str, period_id: str, dataset_id: UUID, path: Pa
     metadata = {
         "survey_id": survey_id,
         "period_id": period_id,
-        "form_types": ['001', '002'], # TODO: generate dynamically eventually
+        "form_types": ["001", "002"],  # TODO: generate dynamically
         "sds_published_at": get_mocked_chronological_date(path.name),
         "total_reporting_units": get_json_file_count_from_path(path),
         "schema_version": "v1.0.0",
@@ -115,22 +116,20 @@ def build_sds_data(*, survey_id: str, period_id: str, dataset_id: UUID, path: Pa
         # "sds_schema_version": 1, # TODO: No longer needed? Not in official data model for SDS
         "filename": "",
         "dataset_id": dataset_id,
-        "title": f"{path.name} (survey_id: {survey_id}) supplementary data",
+        "title": f"{path.parent.name} {path.name} supplementary data (survey_id: {survey_id})",
     }
 
-    unit_data: list[dict] = []
-    for json_file in path.glob("*.json"):
-        json_data = json.loads(json_file.read_text())
-        unit_data.append(json_data)
-
-    metadata['data'] = unit_data
-
-    return Metadata.model_validate(
-        {**metadata}, from_attributes=True
-    )
+    units = {
+        json_file.stem: json.loads(json_file.read_text())
+        for json_file in path.glob("*.json")
+    }
+    metadata["data"] = units
+    return Metadata.model_validate({**metadata}, from_attributes=True)
 
 
-def generate_dataset_id(*, survey_id: str, schema_version: str, dataset_version: str, period_id: str) -> UUID:
+def generate_dataset_id(
+    *, survey_id: str, schema_version: str, dataset_version: str, period_id: str
+) -> UUID:
     """deterministically a generate dataset_id"""
     combined_hash = hashlib.sha256(
         f"{survey_id}_{schema_version}_{dataset_version}_{period_id}".encode("utf-8")
@@ -139,64 +138,34 @@ def generate_dataset_id(*, survey_id: str, schema_version: str, dataset_version:
 
 
 @lru_cache(maxsize=1)
-def load_mock_data(survey_id: str, folder_name: str) -> dict | list:
-
+def load_mock_data() -> list[Metadata]:
     metadata_collection: list[Metadata] = []
-    mock_data_survey_path = MOCK_DATA_PATH / folder_name
 
-    for path in mock_data_survey_path.iterdir():
+    def process_path(survey_id, path):
         dataset_id = generate_dataset_id(
             survey_id=survey_id,
-            schema_version="v1.0.0",  # TODO: Hardcoded for now
+            schema_version="v1.0.0",
             dataset_version=path.name,
-            period_id="202312"
+            period_id=PERIOD_ID,
         )
-        metadata_collection.append(build_sds_data(
-            survey_id=survey_id,
-            period_id="202312",   # TODO: Can get this from the request if needed?
-            dataset_id=dataset_id,
-            path=path,
-        ))
-    print(metadata_collection)
+        metadata_collection.append(
+            build_sds_data(
+                survey_id=survey_id,
+                period_id=PERIOD_ID,
+                dataset_id=dataset_id,
+                path=path,
+            )
+        )
 
+    for survey_mock_path in MOCK_DATA_ROOT_PATH.rglob("*"):
+        if survey_ids := MOCK_DATA_PATHS_BY_SURVEY_ID.get(survey_mock_path.name):
+            if not isinstance(survey_ids, list):
+                survey_ids = [survey_ids]
+            for survey_id in survey_ids:
+                for path in survey_mock_path.iterdir():
+                    process_path(survey_id, path)
 
-        # unit_data = "REMOVE BEFORE RESPONSE IS SENT unit_data" : {"value of identifier 123455 etc": "the actual data from the mocked file json"} # TODO: Add unit data
-
-    # metadata: list[MetadataResponse] = []
-    # mock_data_survey_path = MOCK_DATA_PATH / folder_name
-    #
-    # for schema_path in mock_data_survey_path.rglob("*.json"):
-    #     json_data = json.loads(schema_path.read_text())
-    #     metadata.append(build_sds_data(
-    #         survey_id,
-    #         "202312",   # TODO: Can get this from the request if needed?
-    #         schema_path
-    #     ))
-    #     # unit_data = "REMOVE BEFORE RESPONSE IS SENT unit_data" : {"value of identifier 123455 etc": "the actual data from the mocked file json"} # TODO: Add unit data
-
-
-    # with open(MOCK_DATA_PATH / filename, encoding="utf-8") as mock_data_file:
-    #     return json.load(mock_data_file)
-
-
-def load_mock_sds_dataset_metadata(survey_id: str) -> list[dict]:
-    if folder_name := SURVEY_ID_TO_MOCK_DATA_FOLDER_MAPPING.get(survey_id):
-        return load_mock_data(survey_id, folder_name)
-
-    raise HTTPException(status_code=404)
-
-
-
-
-
-    # survey_id_filename_map = {
-    #     "123": "supplementary_dataset_metadata_response",
-    # }
-    #
-    # if filename := survey_id_filename_map.get(survey_id):
-    #     return load_mock_data(f"mock_data/{filename}.json")
-    #
-    # raise HTTPException(status_code=404)
+    return metadata_collection
 
 
 def encrypt_mock_data(mock_data: MutableMapping) -> MutableMapping:
@@ -209,4 +178,7 @@ def encrypt_mock_data(mock_data: MutableMapping) -> MutableMapping:
 
 if __name__ == "__main__":
     # uvicorn.run(app, host="localhost", port=5003)
-    get_sds_dataset_ids("221")
+    # metadata = get_sds_dataset_ids("221")
+    # print(metadata)
+    data = get_sds_data(UUID("7b049fcb-aec7-83c6-375d-5f166b5f5005"), "34942807969")
+    print(data)
